73c2e6ade65d4eeceddbd47f8389bc2c4ed4b1ad
biggreenrm <biggreen.rm@gmail.com>
2020-03-05 13:20:18 +0300

    [add] user registration form and view.register

--- account/views.py
+++ account/views.py
@@ -2,9 +2,25 @@ from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 # Create your views here.
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Создаём нового пользователя без сохранения в БД
+            new_user = user_form.save(commit=False)
+            # Задаём пользователю зашифрованный пароль
+            new_user.set_password(user_form.cleaned_data["password"])
+            # Сохраняем пользователя в БД
+            new_user.save()
+            return render(request, "account/register_done.html", {"new_user": new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, "account/register.html", {"user_form": user_form})
 
 
 def user_login(request):
