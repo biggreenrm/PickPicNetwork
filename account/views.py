34bcad0694ac9e12b6a08a551c4bf0469ce7b5dc
biggreenrm <biggreen.rm@gmail.com>
2020-03-08 13:40:11 +0300

    add system messages about succes or error in editing user's profile

--- account/views.py
+++ account/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.contrib import messages
 from .models import Profile
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 
@@ -66,6 +67,9 @@ def edit_account(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, "Your profile updated successfully")
+        else:
+            messages.error(request, "Error updating your profile")
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
