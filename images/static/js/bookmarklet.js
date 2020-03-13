a8fa8d59ed56da1bce619cee236fd06b535608ff
biggreenrm <biggreen.rm@gmail.com>
2020-03-13 14:05:24 +0300

    complete bookmarklet

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -1,51 +1,74 @@
 (function () {
     var jquery_version = '3.3.1';
-    var site_url = 'http://127.0.0.1:8000/';
+    var site_url = 'https://cd8f9755.ngrok.io';
     var static_url = site_url + 'static/';
     var min_width = 100;
     var min_height = 100;
 
     function bookmarklet(msg) {
-        // Загрузка CSS-стилей
-        var css = JQuery('<link>');
+        // load CSS
+        var css = jQuery('<link>');
         css.attr({
             rel: 'stylesheet',
             type: 'text/css',
-            href: static_url + 'css/bookmarklet.css?r=' +
-                Math.floor(Math.random() * 99999999999999999999)
+            href: static_url + 'css/bookmarklet.css?r=' + Math.floor(Math.random() * 99999999999999999999)
         });
         jQuery('head').append(css);
-        // загрузка HTML
-        box_html = '<div id="bookmarklet"><a href="#" id="close">&times;</a><h1>Select an image to bookmark:</h1><div class="images"</div></div>';
+
+        // load HTML
+        box_html = '<div id="bookmarklet"><a href="#" id="close">&times;</a><h1>Select an image to bookmark:</h1><div class="images"></div></div>';
         jQuery('body').append(box_html);
-        // Добавление скрытия букмарклета при нажатии на крестик
+
+        // close event
         jQuery('#bookmarklet #close').click(function () {
             jQuery('#bookmarklet').remove();
         });
+
+        // find images and display them
+        jQuery.each(jQuery('img[src$="jpg"]'), function (index, image) {
+            if (jQuery(image).width() >= min_width && jQuery(image).height() >= min_height) {
+                image_url = jQuery(image).attr('src');
+                jQuery('#bookmarklet .images').append('<a href="#"><img src="' + image_url + '" /></a>');
+            }
+        });
+
+        // when an image is selected open URL with it
+        jQuery('#bookmarklet .images a').click(function (e) {
+            selected_image = jQuery(this).children('img').attr('src');
+            // hide bookmarklet
+            jQuery('#bookmarklet').hide();
+            // open new window to submit the image
+            window.open(site_url + 'images/create/?url='
+                + encodeURIComponent(selected_image)
+                + '&title='
+                + encodeURIComponent(jQuery('title').text()),
+                '_blank');
+        });
     };
 
-    // Проверка, подключена ли jQuery. 
-    if (typeofwindow.jQuery != 'undefined') {
+    // Check if jQuery is loaded
+    if (typeof window.jQuery != 'undefined') {
         bookmarklet();
     } else {
-        // Проверка, что атрибут $ окна не занят другим объектом.
+        // Check for conflicts
         var conflict = typeof window.$ != 'undefined';
-        // Создание тега <script> с загрузкой jQuery.
+        // Create the script and point to Google API
         var script = document.createElement('script');
-        script.src = '//ajax.googleapis.com/ajax/libs/jquery/' + jquery_version + '/jquery.min.js';
-        // Добавление тега в блок <head> документа.
+        script.src = '//ajax.googleapis.com/ajax/libs/jquery/' +
+            jquery_version + '/jquery.min.js';
+        // Add the script to the 'head' for processing
         document.head.appendChild(script);
-        // Добавление возможности использовать несколько попыток для загрузки jQuery.
+        // Create a way to wait until script loading
         var attempts = 15;
         (function () {
-            // Проверка, подключена ли jQuery
+            // Check again if jQuery is undefined
             if (typeof window.jQuery == 'undefined') {
                 if (--attempts > 0) {
-                    // Если не подключена, пытаемся снова загрузить
+                    // Calls himself in a few milliseconds
                     window.setTimeout(arguments.callee, 250)
                 } else {
-                    // Превышено число попыток загрузки jQuery, выводим сообщение.
-                    alert('An error occurred while loading jQuery')
+                    // Too much attempts to load, send error
+                    alert('An error ocurred while loading jQuery')
                 }
             } else {
                 bookmarklet();
--- socialnet/settings.py
+++ socialnet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "tgo1!g&butvd&n0*_oi6han458-_%1^v$!0ja=-)pbsmge=yzb"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["mysite.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["mysite.com", "localhost", "127.0.0.1", "cd8f9755.ngrok.io"]
 
 # backends for authentication
 
