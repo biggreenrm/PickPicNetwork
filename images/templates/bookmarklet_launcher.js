2faff7cda4e3c167d611883a1ff9b58b862a5337
biggreenrm <biggreen.rm@gmail.com>
2020-03-19 12:22:16 +0300

    create new folder for common files, create decorator that check if request is ajax or not

--- images/templates/bookmarklet_launcher.js
+++ images/templates/bookmarklet_launcher.js
@@ -3,6 +3,6 @@
         myBookmarklet();
     }
     else {
-        document.body.appendChild(document.createElement('script')).src = 'https://0352c3a0.ngrok.io/static/js/bookmarklet.js?r=' + Math.floor(Math.random() * 99999999999999999999);
+        document.body.appendChild(document.createElement('script')).src = 'https://231cf509.ngrok.io/static/js/bookmarklet.js?r=' + Math.floor(Math.random() * 99999999999999999999);
     }
 })();
--- socialnet/settings.py
+++ socialnet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "tgo1!g&butvd&n0*_oi6han458-_%1^v$!0ja=-)pbsmge=yzb"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["mysite.com", "localhost", "127.0.0.1", "0352c3a0.ngrok.io",]
+ALLOWED_HOSTS = ["mysite.com", "localhost", "127.0.0.1", "231cf509.ngrok.io",]
 
 # backends for authentication
 
